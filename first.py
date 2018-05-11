0349f4072943d487ee2350e81071173e70b3ba45
Ricard Garcia <imricardgarcia@gmail.com>
2018-05-11 21:13:04 +0200

    Another document.

---
+++ first.py
@@ -0,0 +1,29 @@
+# Prints a string
+print("Hello World")
+
+
+#Fill Color 1
+fill(0.3,0.7,1)
+#Drawing a rectangle
+rect(4,6,300,randint(10, 500))
+
+#Fill Color 2
+fill(random(),0.7,0.4)
+
+#Stroke Colour + Width
+stroke(0,0,1)
+strokeWidth(2)
+
+#Draw an ellipse
+oval(500,500,200,180)
+
+
+#ASCII Artq strings must be on a single line, unless you wrap with """
+print("""
+    ||  ||  ||
+        ||
+       ====
+    \\\______//
+        ``````
+
+    """)
