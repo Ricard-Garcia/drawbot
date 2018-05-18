abca58e03f14334a884e55f75463f623e59f1877
Ricard Garcia <imricardgarcia@gmail.com>
2018-05-18 18:40:33 +0200

    First ovals + strokes

--- basics.py
+++ basics.py
@@ -1,4 +1,15 @@
 #Some basics of python in Drawbot
 
-#Draw an ellipse
-oval(500,500,300,50)
+#Colours
+fill(0,0,0.8)
+stroke(1,05,0.4)
+strokeWidth(20)
+
+
+#Draw an ellipses
+oval(500,500,500,500)
+oval(0,500,500,500)
+oval(500,0,500,500)
+
+stroke(None) #Without stroke width
+oval(0,0,500,500)
